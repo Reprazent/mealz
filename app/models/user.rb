6952e97fa4b355cf95c5dc860ef13d40c303b15c
Bob Van Landuyt <bob@vanlanduyt.co>
2015-03-22 17:47:44 +0100

    Add the relationshit meals >-< users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates :balance, presence: true
   has_many :payed_for_meals, class_name: Meal.name, foreign_key: :payed_by_id
+  has_and_belongs_to_many :meals
 end
