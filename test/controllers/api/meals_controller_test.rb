5f65595d769e7a284288aa7de4de1f6d777bff4b
Bob Van Landuyt <bob@vanlanduyt.co>
2015-03-29 16:46:19 +0200

    Remove debug line

--- test/controllers/api/meals_controller_test.rb
+++ test/controllers/api/meals_controller_test.rb
@@ -13,7 +13,6 @@ describe Api::MealsController do
     meal_params = { meal: { eater_names: ["pjaspers ", "atog", "TomKlaasen", "tomklaasen", "Reprazent"], payed_by_username: "reprazent", amount: "7,9" } }
     post :create, meal_params.merge(format: :json)
     body = JSON.parse(response.body)
-    puts body.inspect
     assert_equal 5.925, body["meal"]["payed_by"]["balance"]
   end
 end
