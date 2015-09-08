979e321830f84d0e64c6d78918e5707aef987cee
Bob Van Landuyt <bob@vanlanduyt.co>
2015-09-08 20:30:44 +0200

    Add the API endpoint to archive users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,5 +5,12 @@ module Api
       @users = User.unarchived.order("balance ASC")
       respond_with @users, location: nil, root: false
     end
+
+    def destroy
+      @user = User.find_by_username(params[:id].downcase.strip)
+      return render nothing: true, status: :not_found unless @user
+      @user.archive!
+      respond_with @user, location: nil
+    end
   end
 end
--- test/controllers/api/users_controller_test.rb
+++ test/controllers/api/users_controller_test.rb
@@ -10,4 +10,16 @@ describe Api::UsersController do
     get :index, format: :json
     assert_equal 2, JSON.parse(response.body).size
   end
+
+  it "can archive a user" do
+    @user = @users.first
+    delete :destroy, id: @user.username, format: :json
+    assert_response :success
+    assert @user.reload.archived_at
+  end
+
+  it "replies 404 when applicable" do
+    delete :destroy, id: "bloe" , format: :json
+    assert_response :not_found
+  end
 end
